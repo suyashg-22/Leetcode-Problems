3c2ae887b05cb90a521d392612726216611636cf
Suyash GHilahare <suyashghilahare123@gmail.com>
2024-08-09 19:33:05 +0530

    Time: 3 ms (71.37%), Space: 12.4 MB (30.01%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int ans =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]== target){
+                ans =i;
+                break;
+            }
+            else if(nums[i]> target){
+                ans =i;
+                break;
+            }
+            else{
+                ans =nums.size();
+            }
+        }
+        return ans;
+    }
+};
