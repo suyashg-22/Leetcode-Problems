8ad153cbfa623a6b386ef03fb59bbd06ad5c3e5b
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-04-11 07:37:45 +0530

    Time: 0 ms (100%), Space: 21.6 MB (99.64%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,11 +4,10 @@ public:
         int n=nums.size();
         int xor1 =0;
         int xor2 =0;
-        for(int i=1;i<=n;i++){
-            xor2 = xor2^i;
-        }
+
         for(int i=0;i<n;i++){
             xor1 = xor1 ^ nums[i];
+            xor2 = xor2^(i+1);
         }
         return xor1 ^ xor2;
     }
