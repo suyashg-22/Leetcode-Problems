f25efeece7b0f3decff68af7acb750f9749b2d14
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-05-21 01:59:53 +0530

    Time: 0 ms (100%), Space: 94.9 MB (93.46%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n =nums1.size();
-        int m = nums2.size();
-        int i=0;
-        int j=0;
-        vector<int>arr;
-        while(i<n && j<m){
-            if(nums1[i]<=nums2[j]){
-                arr.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                arr.push_back(nums2[j]);
-                j++;
-            }
+       int n1 = nums1.size(), n2 = nums2.size();
+    //if n1 is bigger swap the arrays:
+    if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+
+    int n = n1 + n2; //total length
+    int left = (n1 + n2 + 1) / 2; //length of left half
+    //apply binary search:
+    int low = 0, high = n1;
+    while (low <= high) {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        //calculate l1, l2, r1 and r2;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < n1) r1 = nums1[mid1];
+        if (mid2 < n2) r2 = nums2[mid2];
+        if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1) {
+            if (n % 2 == 1) return max(l1, l2);
+            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
         }
-        while(i<n){
-            arr.push_back(nums1[i]);
-            i++;
-        }
-        while(j<m){
-            arr.push_back(nums2[j]);
-            j++;
-        }
-        int x = arr.size();
-        int mid = arr.size()/2;
-        long double ans =0;
-        if(x%2==0 ){
-            if(mid!=0) ans =(arr[mid]+arr[mid-1])/2.0;
-    
-        }    
-        else ans=arr[mid];
-        return ans;
+
+        //eliminate the halves:
+        else if (l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return 0; //dummy statement
     }
 };
