c5aeef7b58de1bb04ce996254f261a12aa6bef6a
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-06-18 10:20:41 +0530

    Time: 33 ms (84.16%), Space: 18.6 MB (24.29%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,14 +11,15 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode* t1 = headA;
         ListNode* t2 = headB;
-        while(t1 && t2){
-            while(t2){
-                if(t1==t2) return t1;
-                t2=t2->next;
-            }
-            t1= t1->next;
-            t2=headB;
+        if(!t1 && !t2) return NULL;
+        while(t1!=t2){
+            t1=t1->next;
+            t2=t2->next;
+            if(t1==t2) return t1;
+            if(!t1) t1=headB;
+            if(!t2) t2=headA;
+
         }
-        return NULL;
+        return t1;
     }
 };
