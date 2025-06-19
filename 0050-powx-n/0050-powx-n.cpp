9fe9e1935ed55666ca950421c563be531f49a864
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-06-19 18:14:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return (double) pow(x,n);
+        double ans = 1;
+        bool flag = true;
+        if(n<0) {
+            flag = false;
+            n=abs(n);
+        }
+        while(n>0){
+            if(n%2==1){
+                ans=ans*x;
+                n=n-1;
+            }
+            else{
+                x=x*x;
+                n=n/2;
+            }
+        }
+        if(!flag) return 1.0/ans;    
+        return ans;
     }
 };
