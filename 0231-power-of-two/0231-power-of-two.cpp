d2b7c09c87d4f7e015e1155b5c150c1300c4eeca
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-07-18 11:17:08 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.08%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
-    
-    bool recur(int n,int i){
-        if(i>30){
-            return false;
-        }   
-        int check = static_cast<int>(pow(2,i));
-        if(check==n){
-            return true;
-        }
-        return recur(n,i+1);
-    }
-    
     bool isPowerOfTwo(int n) {
-        return recur(n,0);
+        if(n<=0)return false;
+        if(((n)&(n-1))==0) return true;
+        return false;
     }
 };
