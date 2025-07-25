9a74cd74d2801acac18e6452476ef016b717dec0
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-07-25 17:56:20 +0530

    Time: 0 ms (100%), Space: 11.9 MB (49.06%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    string longpref(vector<string>& strs,string& str1,string& str2,int i){
-        string out="";
-        if(i>=strs.size()){
-            return str1;
-        }
-        int n1 = str1.size();
-        int n2 = str2.size();
-        int n = min(n1,n2);
-        for(int j=0;j<n;j++){
-            if(str1[j]==str2[j]){
-                out+=str1[j];
-            }
-            
-        }
-        return longpref(strs,out,strs[i+1],i+1);
-    }
-
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.size()==1){
-            return strs[0];
+        int n =strs.size();
+        int mini =INT_MAX;
+        for(int i=0;i<n;i++){
+            int size = strs[i].size();
+            mini=min(mini,size);
         }
-        return longpref(strs,strs[0],strs[1],0);
+        string ans ="";
+        while(mini>0){
+            bool flag = true;
+            for(int i=0;i<n-1;i++){
+                if(strs[i].substr(0,mini)!=strs[i+1].substr(0,mini)){
+                    mini-=1;
+                    flag =false;
+                    break;
+                }
+            }
+            if(flag){
+                ans = strs[0].substr(0,mini);
+                return ans;
+            }
+        }
+        return ans;
     }
 };
