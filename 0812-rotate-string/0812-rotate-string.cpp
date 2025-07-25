0b38ee8e80060a3d83269224b70a39531be9e54a
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-07-25 23:56:32 +0530

    Time: 0 ms (100%), Space: 8.1 MB (62%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -5,28 +5,6 @@ public:
         int m=goal.size();
         if(n!=m)return false;
 
-        int i=0;
-        string ans ="";
-        int size =m;
-        while(size>0){
-            string temp = goal.substr(0,size);
-            if(s.find(temp)==string::npos){
-                size-=1;
-            }
-            else{
-                i=s.find(temp);
-                break;
-            }
-        }
-        if(i==0 && size==m)return true;
-        i=0;
-        int j= size;
-        while(i<n && j<m){
-            if(s[i]!=goal[j])return false;
-            i+=1;
-            j+=1;
-        }
-        return true;
-        
+        return ((s+s).find(goal)<n);
     }
 };
