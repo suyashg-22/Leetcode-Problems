3f39c6465ae489abdf569878fd7a6bda38a33153
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-07-26 00:07:07 +0530

    Time: 0 ms (100%), Space: 10 MB (11.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        if(s.length() != t.length()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-            
-        int i=0;
-        int j=0;
-        while(i<s.length()&&j<t.length()){
-            if(s[i] != t[i]) return false;
-                i++;
-                j++;
+        int n=s.size();
+        int m =t.size();
+        if(n!=m)return false;
+
+        unordered_map<char,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[s[i]]+=1;
+        }
+        for(int i=0;i<m;i++){
+            if(mpp[t[i]]==0){
+                return false;
             }
+            if(mpp[t[i]]!=0){
+                mpp[t[i]]-=1;
+            }
+        }
         return true;
     }
 };
