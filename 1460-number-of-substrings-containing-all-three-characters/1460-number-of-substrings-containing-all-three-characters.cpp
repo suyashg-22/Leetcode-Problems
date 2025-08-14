7a6bac4a298eaaa596e86862a078826d846a3661
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-08-14 13:07:28 +0530

    Time: 23 ms (28.2%), Space: 11.3 MB (21.76%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    long long f(string s,long long k){
-        long long n =s.size();
-        long long l =0;
-        long long r =0;
-        unordered_map<char,long long>mpp;
-        long long cnt =0;
-        while(r<n){
-            mpp[s[r]]+=1;
-            while(mpp.size()>k){
-                mpp[s[l]]-=1;
-                if(mpp[s[l]]==0)mpp.erase(s[l]);
-                l+=1;
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        unordered_map<char,int>mpp;
+        mpp['a']=-1;
+        mpp['b']=-1;
+        mpp['c']=-1;
+        int cnt =0;
+        for(int i=0;i<n;i++){
+            mpp[s[i]]=i;
+            if(mpp['a']>-1 && mpp['b']>-1 && mpp['c']>-1){
+                cnt+= min({mpp['a'],mpp['b'],mpp['c']})+1;
             }
-            cnt+=r-l+1;
-            r+=1;
         }
         return cnt;
-    }
-    int numberOfSubstrings(string s) {
-        long long n =s.size();
-
-        return n*(n+1)/2-f(s,2);
     }
 };
