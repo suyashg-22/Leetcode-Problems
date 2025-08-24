438b130a26280139eec497a171a9912b7c71bda9
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-08-24 11:17:08 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
+    bool check(vector<int>& nums,int mid,int target){
+        int n = nums.size();
+        if (nums[mid]==target)return 1;
+        if(nums[0]<=nums[mid] && nums[mid]>=nums[n-1]){
+            if(target>=nums[0] && target<=nums[mid])return 1;
+            return 0;
+        } 
+        else if(nums[0]<=nums[mid]){
+            if(target>=nums[0] && target<=nums[mid])return 1;
+            return 0;
+        }
+        else {
+            if(target>=nums[mid] && target<=nums[n-1])return 0;
+            return 1;
+        }
+    }
     int search(vector<int>& nums, int target) {
         int n =nums.size();
-        int low =0;
-        int high =n-1;
-        while(low<=high){
-            int mid = low+ (high-low)/2;
-            if(nums[mid]==target) return mid;
-            //left sorted
-            if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<=nums[mid]) high = mid-1;
-                else low=mid+1;
+        int l =0;
+        int h = n-1;
+        int ans =0;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(check(nums,mid,target)==1){
+                ans = mid;
+                h=mid-1;
             }
-            //right sorted
-            else{
-                if(target<=nums[high] && target>= nums[mid]) low=mid+1;
-                else high=mid-1;
-            }
+            else l=mid+1;
         }
-        return -1;
+        return (nums[ans]==target?ans:-1);
     }
 };
