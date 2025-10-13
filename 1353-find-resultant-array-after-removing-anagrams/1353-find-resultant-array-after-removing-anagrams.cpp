421655c935beb0fb49c34e6c2c18a3ecde85249b
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-10-13 19:08:28 +0530

    Time: 3 ms (48.84%), Space: 16.1 MB (75.29%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -6,6 +6,10 @@ public:
         arr.push_back(words[0]);
         for(int i=1;i<n;i++){
             string s = words[i];
+            if(words[i-1].size()!=words[i].size()){
+                arr.push_back(s);
+                continue;
+            }
             sort(words[i-1].begin(),words[i-1].end());
             sort(words[i].begin(),words[i].end());
             if(words[i-1]==words[i]){
