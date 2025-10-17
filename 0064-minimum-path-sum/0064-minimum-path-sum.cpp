ee9da5c6fd800128507f599094ab0f01e100c1e1
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-10-17 14:35:13 +0530

    Time: 0 ms (100%), Space: 15.4 MB (85.56%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>>*arr;
     int n;
     int m;
     int dp[201][201];
-    int rec(int r,int c){
+    int rec(int r,int c,vector<vector<int>>&grid){
         if(r<0 && c<0)return INT_MAX;
-        if(r==0 && c==0)return (*arr)[0][0];
+        if(r==0 && c==0)return grid[0][0];
         if(dp[r][c]!=-1)return dp[r][c];
 
         int ans = INT_MAX;
         if(r>0){
-            ans = (*arr)[r][c]+rec(r-1,c);
+            ans = grid[r][c]+rec(r-1,c,grid);
         }
         if(c>0){
-            ans = min(ans,(*arr)[r][c]+rec(r,c-1));
+            ans = min(ans,grid[r][c]+rec(r,c-1,grid));
         }
         return dp[r][c]=ans;
     }
+
     int minPathSum(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        arr= &grid;
         memset(dp,-1,sizeof(dp));
-        return rec(n-1,m-1);
+        return rec(n-1,m-1,grid);
     }
 };
