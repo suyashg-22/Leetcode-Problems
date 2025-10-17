857ec7b8fcf6f869ace3f49877d8c84ee81bae1f
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-10-17 21:20:54 +0530

    Time: 0 ms (100%), Space: 8 MB (13.96%) - LeetHub

---
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[1001];
+    int rec(int n){
+        if(n==0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        int ans = 0;
+        for(int i=1;i<n;i++){
+            if(n%i==0 && rec(n-i)==0){
+                ans =1;
+                break;
+            }
+        }
+        return dp[n]=ans;
+    }
+    bool divisorGame(int n) {
+        memset(dp,-1,sizeof(dp));
+        return rec(n);
+    }
+};
