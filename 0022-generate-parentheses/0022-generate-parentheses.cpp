b1c773b1c8f71303c97aa7bffcd136f2dff5e533
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-10-23 11:50:57 +0530

    Time: 1406 ms (5.06%), Space: 538 MB (5.01%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -3,8 +3,7 @@ public:
     vector<string>ans;
     vector<string>rec(int n1,int n2,string &s){
         if(n1>n2){
-            n1 =-1;
-            n2 =-1;
+            return ans;
         }
 
         if(n1<=0 && n2<=0){
