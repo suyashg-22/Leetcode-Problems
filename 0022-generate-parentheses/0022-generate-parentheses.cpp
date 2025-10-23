59c2d4d87eabbe13b682d758f0e0ff98e57c866b
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-10-23 10:30:56 +0530

    Time: 1429 ms (5.06%), Space: 538.1 MB (5.01%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string>ans;
+    vector<string>rec(int n1,int n2,string &s){
+        if(n1>n2){
+            n1 =-1;
+            n2 =-1;
+        }
+
+        if(n1<=0 && n2<=0){
+            if(n1!=-1 && n2!=-1){
+                ans.push_back(s);
+            }
+            return ans;
+        }
+
+        if(n1>0){
+            s.push_back('(');
+            rec(n1-1,n2,s);
+            s.pop_back();
+        }
+        if(n2>0){
+            s.push_back(')'); 
+            rec(n1,n2-1,s);
+            s.pop_back();
+        }
+
+        return ans;
+    }
+    vector<string> generateParenthesis(int n) {
+        string s ="";
+        return rec(n,n,s);
+    }
+};
