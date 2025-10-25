984a3c2e4a0f2bb32cbd448e14e8bd9529820f4c
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-10-25 11:24:07 +0530

    Time: 27 ms (72.19%), Space: 10 MB (83.32%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
-    string S ;
-    int n;
-    int dp[501][501];
-    int rec(int l,int r){
+    int rec(int l,int r,string &s,int dp[][501]){
         if(l>r){
             return 0;
         }
@@ -11,17 +8,19 @@ public:
         if(dp[l][r]!=-1)return dp[l][r];
 
         int ans =1;
-        ans=max(ans,rec(l,r-1));
-        ans=max(ans,rec(l+1,r));
-        if(S[l]==S[r]){
-            ans =max(ans,2+rec(l+1,r-1));
+        ans=max(ans,rec(l,r-1,s,dp));
+        ans=max(ans,rec(l+1,r,s,dp));
+        if(s[l]==s[r]){
+            ans =max(ans,2+rec(l+1,r-1,s,dp));
         }
         return dp[l][r]=ans;
     }
     int minInsertions(string s) {
+        int n;
+        int dp[501][501];
         n=s.size();
-        S=s;
+
         memset(dp,-1,sizeof(dp));
-        return n-rec(0,n-1);
+        return n-rec(0,n-1,s,dp);
     }
 };
