5b8e58b13576ecf73d21082785b40b4c29ed9c30
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-11-02 13:20:48 +0530

    Time: 30 ms (43.19%), Space: 22.6 MB (68.32%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,26 +1,27 @@
+using ll = long long ;
 class Solution {
 public:
-    int n;
-    int dp[301][5001];
-    vector<int>*arr;
-    int rec(int level,int sleft){
-        if(sleft==0){
-            return 1;
-        }
-        if(level==n){
-            if(sleft==0)return 1;
-            return 0;
-        }
-        if(dp[level][sleft]!=-1)return dp[level][sleft];
-        int ans = rec(level + 1, sleft); 
-        if((*arr)[level] <= sleft)
-            ans += rec(level, sleft - (*arr)[level]); 
-        return dp[level][sleft]=ans;
-    }
+    ll dp[301][5001];
     int change(int amount, vector<int>& coins) {
-        n = coins.size();
-        memset(dp,-1,sizeof(dp));
-        arr=&coins;
-        return rec(0,amount);
+        int n =coins.size();
+        for(int l = n;l>=0;l--){
+            for(int sum =0;sum<=amount;sum++){
+                if(sum==0){
+                    dp[l][sum]=1;
+                }
+                else if(l==n){
+                    dp[l][sum]=0;
+                }
+                else{
+
+                    int ans = dp[l+1][sum];
+                    if(coins[l]<=sum){
+                        ans+= dp[l][sum-coins[l]];
+                    }
+                    dp[l][sum]=ans;
+                }
+            }
+        }
+        return dp[0][amount];
     }
 };
