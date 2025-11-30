0e69c4870972ee00a031edd65299d78e757f302d
Suyash GHilahare <suyashghilahare123@gmail.com>
2025-12-01 00:00:46 +0530

    Time: 15 ms (38.81%), Space: 33.2 MB (35.31%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,65 +1,41 @@
+int dp[40001][3];
 class Solution {
 public:
+    int n;
+    vector<int>* arr;
+    const int NEG_INF = -1000000000;
+
+    int rec(int level, int sumr) {
+        // base case FIRST
+        if (level == n) {
+            if (sumr == 0) return 0;
+            return NEG_INF;
+        }
+
+        // now safe to check dp
+        if (dp[level][sumr] != INT_MIN)
+            return dp[level][sumr];
+
+        int temp = (*arr)[level];
+
+        // Option 1: skip
+        int ans = rec(level + 1, sumr);
+
+        // Option 2: take
+        ans = max(ans, temp + rec(level + 1, (sumr + temp) % 3));
+
+        return dp[level][sumr] = ans;
+    }
 
     int maxSumDivThree(vector<int>& nums) {
-        int n = nums.size();
-        int sum =0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
-        if(sum%3==0)return sum;
-        else if(sum%3==1){
-            int two1=-1;
-            int two2=-1;
-            int one =-1;
-            int cntone=0;
-            int cnttwo =0;
-            sort(nums.begin(),nums.end());
-            for(int i=0;i<n;i++){
-                if(nums[i]%3==2 && cnttwo<2){
-                    if(two1==-1){
-                        two1=nums[i];
-                    }
-                    else{
-                        two2=nums[i];
-                    }
-                    cnttwo+=1;
-                }
-                else if(nums[i]%3==1 && cntone<1){
-                    one = nums[i];
-                    cntone+=1;
-                }
-            }
-            if(cnttwo<2)return sum-one;
-            if(cntone<1)return sum-two1-two2;
-            return max(sum-one,sum-two1-two2);
-        }
-        else{
-            int one1=-1;
-            int one2=-1;
-            int two =-1;
-            int cntone =0;
-            int cnttwo =0;
-            sort(nums.begin(),nums.end());
-            for(int i=0;i<n;i++){
-                if(nums[i]%3==1 && cntone<2){
-                    if(one1==-1){
-                        one1 =nums[i];
-                    }
-                    else{
-                        one2 =nums[i];
-                    }
-                    cntone+=1;
-                }
-                else if(nums[i]%3==2 && cnttwo<1){
-                    two = nums[i];
-                    cnttwo+=1;
-                }
-            }
-            if(cntone<2)return sum-two;
-            if(cnttwo<1)return sum-one1-one2;
-            return max(sum-one1-one2,sum-two);
-        }
-        return 0;
+        n = nums.size();
+        arr = &nums;
+
+        // initialize dp with INT_MIN
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j < 3; j++)
+                dp[i][j] = INT_MIN;
+
+        return rec(0, 0);
     }
 };
