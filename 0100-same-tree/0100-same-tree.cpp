35025f3589094a475efa861e244404717ec2007e
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-01-09 18:56:16 +0530

    Time: 0 ms (100%), Space: 12.9 MB (13.53%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,8 +11,18 @@
  */
 class Solution {
 public:
+    bool rec(TreeNode* p,TreeNode* q){
+        if(!p && !q) return true;
+        if((p && !q)||(!p &&q))return false;
+        if(p->val != q->val)return false;
+
+        bool l = rec(p->left,q->left);
+        bool r = rec(p->right,q->right);
+
+        if(!l || !r)return false;
+        else return true;
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL || q==NULL) return (p==q);
-        return (p->val==q->val)&&(isSameTree(p->left,q->left))&&(isSameTree(p->right,q->right));
+        return rec(p,q);
     }
 };
