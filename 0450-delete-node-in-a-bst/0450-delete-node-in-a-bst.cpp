3f5339682c16cad0c6df5bc68eaca442ee82b1d5
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-01-12 02:29:58 +0530

    Time: 0 ms (100%), Space: 34.4 MB (37.67%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,41 +11,84 @@
  */
 class Solution {
 public:
-    TreeNode* f (TreeNode*root){
-        if(!root->right) return root;
-        return f(root->right);
-    }
-    TreeNode * helper(TreeNode* root){
-        if(!root->left) return root->right;
-        else if(!root->right) return root->left;
-
-        TreeNode* rightchild = root->right;
-        TreeNode* mostright = f(root->left);
-        mostright->right=rightchild;
-
-        return root->left;
-    }
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if(!root) return NULL;
-        if(root->val==key) return helper(root);
-
-        TreeNode * temp = root;
-        while(temp){
-            if(temp->val>key){
-                if(temp->left && temp->left->val==key){
-                    temp->left=helper(temp->left);
-                    break;
-                }
-                else temp=temp->left;
+        TreeNode* node ;
+        TreeNode* head ;
+    void del(TreeNode* node,TreeNode* temp,TreeNode* templ,TreeNode*tempr,int dir){
+        if(node==temp){
+            head->left=NULL;
+            head->right=NULL;
+            if(!templ && !tempr){
+                head=NULL;
+                return;
+            }
+            else if(!templ){
+                head=tempr;
+            }
+            else if(!tempr){
+                head=templ;
             }
             else{
-                if(temp->right && temp->right->val==key){
-                    temp->right=helper(temp->right);
-                    break;
+                head=tempr;
+                while(tempr->left){
+                    tempr=tempr->left;
                 }
-                else temp = temp->right;
+                tempr->left=templ;
             }
         }
-        return root;
+        else{
+            temp->left=NULL;
+            temp->right=NULL;
+            if(dir==0)node->left=NULL;
+            else node->right=NULL;
+            
+            if(!templ && !tempr){
+                return;
+            }
+            else if(!templ){
+                if(dir==0)node->left=tempr;
+                else node->right=tempr;
+            }
+            else if(!tempr){
+                if(dir==0)node->left=templ;
+                else node->right=templ;
+            }
+            else{
+                TreeNode* consttr=tempr;
+                while(tempr->left){
+                    tempr=tempr->left;
+                }
+                tempr->left=templ;
+                if(dir==0)node->left=consttr;
+                else node->right=consttr;
+            }
+        }
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        node=root;
+        head=root;
+        if(!node){
+            return NULL;
+        }
+        if(node->val==key){
+            del(node,node,node->left,node->right,-1);
+        }
+        while(node){
+            if(node->val>key){
+                if(node->left && node->left->val==key){
+                    del(node,node->left,node->left->left,node->left->right,0);
+                    break;
+                }
+                else node=node->left;
+            }
+            else{
+                if(node->right && node->right->val==key){
+                    del(node,node->right,node->right->left,node->right->right,1);
+                    break;
+                }
+                else node=node->right;
+            }
+        }
+        return head;
+
     }
 };
