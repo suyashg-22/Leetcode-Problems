b44672355213b0721100aaf6c5835ffaa7392bed
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-01-12 16:01:43 +0530

    Time: 0 ms (100%), Space: 24.3 MB (65.99%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,15 +11,78 @@
  */
 class Solution {
 public:
-    vector<int>ans;
-    void rec(TreeNode*node){
-        if(node->left)rec(node->left);
-        ans.push_back(node->val);
-        if(node->right)rec(node->right);
-
+    TreeNode* node ;
+    TreeNode* head ;
+    TreeNode* ans =NULL;
+    void del(TreeNode* node,TreeNode* temp,TreeNode* templ,TreeNode*tempr,int dir){
+        if(node==temp){
+            head->left=NULL;
+            head->right=NULL;
+            if(!templ && !tempr){
+                head=NULL;
+                return;
+            }
+            else if(!templ){
+                head=tempr;
+            }
+            else if(!tempr){
+                head=templ;
+            }
+            else{
+                head=tempr;
+                while(tempr->left){
+                    tempr=tempr->left;
+                }
+                tempr->left=templ;
+            }
+        }
+        else{
+            temp->left=NULL;
+            temp->right=NULL;
+            if(dir==0)node->left=NULL;
+            else node->right=NULL;
+            
+            if(!templ && !tempr){
+                return;
+            }
+            else if(!templ){
+                if(dir==0)node->left=tempr;
+                else node->right=tempr;
+            }
+            else if(!tempr){
+                if(dir==0)node->left=templ;
+                else node->right=templ;
+            }
+            else{
+                TreeNode* consttr=tempr;
+                while(tempr->left){
+                    tempr=tempr->left;
+                }
+                tempr->left=templ;
+                if(dir==0)node->left=consttr;
+                else node->right=consttr;
+            }
+        }
     }
+
     int kthSmallest(TreeNode* root, int k) {
-        rec(root);
-        return ans[k-1];
+        node =root;
+        head = root;
+        while(k--){
+            if(!node->left){
+                ans=node;
+                del(node,node,node->left,node->right,-1);
+                node=head;
+            }
+            else{
+                while(node->left && node->left->left){
+                    node=node->left;
+                }
+                ans=node->left;
+                del(node,node->left,node->left->left,node->left->right,0);
+                node=head;
+            }
+        }
+        return ans->val;
     }
 };
