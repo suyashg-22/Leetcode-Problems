9c16f4dbe93a3db4f402f7063982b7b6445801f3
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-01-14 16:21:46 +0530

    Time: 0 ms (100%), Space: 18.6 MB (8.68%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc]==color)return image;
+
         int start = image[sr][sc];
         int n = image[0].size();
         int m = image.size();
@@ -9,13 +11,12 @@ public:
         vector<pair<int, int>> padosi{{-1, 0}, {+1, 0}, {0, -1}, {0, +1}};
         vector<vector<int>>vis(m,vector<int>(n,0));
         q.push({sr,sc});
-        image[sr][sc]=color;
 
         while(!q.empty()){
             auto p = q.front();
             auto x = p.first;
             auto y = p.second;
-            vis[x][y]=1;
+            image[x][y]=color;
             q.pop();
             for(int i=0;i<padosi.size();i++){
                 int a = x+padosi[i].first;
@@ -23,11 +24,8 @@ public:
                 pair<int,int>z = {a,b};
                 if(a>=0 && a<m && b>=0 && b<n){
                     if(image[a][b]==start){
-                        if(vis[a][b]==0){
-                            vis[a][b]=1;
                             q.push(z);
                             image[a][b]=color;
-                        }
                     }
                 }
             }
