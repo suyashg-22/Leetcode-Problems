ffae08586b6ac70176572901e1ba0baa7a13b60f
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-02-01 01:01:11 +0530

    Time: 67 ms (27.33%), Space: 13.8 MB (85.8%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -5,35 +5,63 @@ public:
     int dp [71][71][71];
     vector<vector<int>>*arr;
 
-    int rec(int i , int j1,int j2){
-        if(i==r-1){
-            if(j1==j2){
-                return (*arr)[i][j1];
-            }
-            else return (*arr)[i][j1]+(*arr)[i][j2];
-        }
+    // int rec(int i , int j1,int j2){
+    //     if(i==r-1){
+    //         if(j1==j2){
+    //             return (*arr)[i][j1];
+    //         }
+    //         else return (*arr)[i][j1]+(*arr)[i][j2];
+    //     }
 
-        if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
-        int ans =0;
-        for(auto it:dy){
-            for(auto jt:dy){
-                int nj1 = j1+it;
-                int nj2 = j2+jt;
-                if(nj1>=0 && nj1<c && nj2>=0 && nj2<c){
-                    if(j1==j2){
-                        ans=max(ans,(*arr)[i][j1]+rec(i+1,nj1,nj2));
-                    }
-                    else ans = max(ans,(*arr)[i][j1]+(*arr)[i][j2]+rec(i+1,nj1,nj2));
-                }
-            }
-        }
-        return dp[i][j1][j2]=ans;
-    }
+    //     if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
+    //     int ans =0;
+    //     for(auto it:dy){
+    //         for(auto jt:dy){
+    //             int nj1 = j1+it;
+    //             int nj2 = j2+jt;
+    //             if(nj1>=0 && nj1<c && nj2>=0 && nj2<c){
+    //                 if(j1==j2){
+    //                     ans=max(ans,(*arr)[i][j1]+rec(i+1,nj1,nj2));
+    //                 }
+    //                 else ans = max(ans,(*arr)[i][j1]+(*arr)[i][j2]+rec(i+1,nj1,nj2));
+    //             }
+    //         }
+    //     }
+    //     return dp[i][j1][j2]=ans;
+    // }
     int cherryPickup(vector<vector<int>>& grid) {
         this->r = grid.size();
         this->c = grid[0].size();
         this->arr=&grid;
-        memset(dp,-1,sizeof(dp));
-        return rec(0,0,c-1);
+        // memset(dp,-1,sizeof(dp));
+        // return rec(0,0,c-1);
+
+        for(int i=r-1;i>=0;i--){
+            for(int j1=c-1;j1>=0;j1--){
+                for(int j2=c-1;j2>=0;j2--){
+                    if(i==r-1){
+                        if(j1==j2)dp[i][j1][j2]=grid[i][j1];
+                        else{
+                            dp[i][j1][j2]=grid[i][j1]+grid[i][j2];
+                        }
+                    }
+                    else{
+                        int ans =0;
+                        for(auto it:dy){
+                            for(auto jt:dy){
+                                int nj1 = j1+it;
+                                int nj2 = j2+jt;
+                                if(nj1>=0 && nj1<c && nj2>=0 && nj2<c){
+                                    if(j1==j2)ans=max(ans,grid[i][j1]+dp[i+1][nj1][nj2]);
+                                    else ans=max(ans,grid[i][j1]+grid[i][j2]+dp[i+1][nj1][nj2]);
+                                }
+                            }
+                        }
+                        dp[i][j1][j2]=ans;
+                    }
+                }
+            }
+        }
+        return dp[0][0][c-1];
     }
 };
