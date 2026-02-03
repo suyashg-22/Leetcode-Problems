08471d023f3e09eca169ac1d7967f8623d1ee964
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-02-04 01:36:34 +0530

    Time: 28 ms (53.69%), Space: 22.6 MB (65.93%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,25 @@
-using ll = long long ;
+using ll = long long;
 class Solution {
 public:
     ll dp[301][5001];
     int change(int amount, vector<int>& coins) {
-        int n =coins.size();
-        for(int l = n;l>=0;l--){
-            for(int sum =0;sum<=amount;sum++){
-                if(sum==0){
-                    dp[l][sum]=1;
-                }
-                else if(l==n){
-                    dp[l][sum]=0;
-                }
-                else{
+        int n = coins.size();
+        for (int l = n; l >= 0; l--) {
+            for (int sum = 0; sum <= amount; sum++) {
 
-                    int ans = dp[l+1][sum];
-                    if(coins[l]<=sum){
-                        ans+= dp[l][sum-coins[l]];
+                if (l == n) {
+                    if (sum == 0) {
+                        dp[l][sum] = 1;
+                    } else
+                        dp[l][sum] = 0;
+                }
+                 else {
+
+                    int ans = dp[l + 1][sum];
+                    if (coins[l] <= sum) {
+                        ans += dp[l][sum - coins[l]];
                     }
-                    dp[l][sum]=ans;
+                    dp[l][sum] = ans;
                 }
             }
         }
