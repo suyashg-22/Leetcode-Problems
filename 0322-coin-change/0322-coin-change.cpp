fb837800f030145f06c0d3463d0a05485290fb0b
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-02-03 23:50:11 +0530

    Time: 614 ms (5.02%), Space: 13 MB (98.67%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[13][10001];
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+
+        for(int level=n;level>=0;level--){
+            for(int val=amount;val>=0;val--){
+                if(level==n){
+                    if(val==amount)dp[level][val]=0;
+                    else dp[level][val]=1e9;
+                }
+                else{
+                    int ans =dp[level+1][val];
+                    int t = (amount-val)/coins[level];
+                    for(int i=0;i<t;i++){
+                        ans=min(ans,1+dp[level][val+coins[level]]);
+                    }
+                    dp[level][val]=ans;
+                }
+            }
+        }
+        if(dp[0][0]>=1e9)return -1;
+        return dp[0][0];
+    }
+};
