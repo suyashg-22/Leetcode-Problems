d8a868d3160947f5320abbcfc363d00624f84165
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-03-07 23:50:21 +0530

    Time: 90 ms (37%), Space: 13.2 MB (84.45%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -5,15 +5,17 @@ public:
     int dp[1001][1001];
 
     int rec(int i, int j){
-        if(i==n || j==-1){
+        if(i>j){
             return 0;
         }
+        if(i==j)return 1;
+
         if(dp[i][j]!=-1)return dp[i][j];
         int ans =0;
         ans = max(ans,rec(i+1,j));
         ans = max(ans,rec(i,j-1));
         if(s[i]==s[j]){
-            ans = max(ans,1+rec(i+1,j-1));
+            ans = max(ans,2+rec(i+1,j-1));
         }
         return dp[i][j]=ans;
     }
