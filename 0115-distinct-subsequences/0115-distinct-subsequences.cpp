eb7e1a4f71868f1ea221d16b7952176e3f35b9a0
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-03-16 18:49:31 +0530

    Time: 41 ms (16.35%), Space: 17.4 MB (75.64%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,22 +1,24 @@
+using ll = long long;
+const ll mod = 1e9+9;
 class Solution {
 public:
     string s;
     string t;
     int n1;
     int n2;
-    int dp[1001][1001];
+    ll dp[1001][1001];
 
-    int rec(int level,int c){
-        if(c==n2)return 1;
-        if(level==n1)return 0;
-        if(dp[level][c]!=-1)return dp[level][c];
+    // int rec(int level,int c){
+    //     if(c==n2)return 1;
+    //     if(level==n1)return 0;
+    //     if(dp[level][c]!=-1)return dp[level][c];
 
-        int ans=rec(level+1,c);
-        if(t[c]==s[level]){
-            ans+= rec(level+1,c+1);
-        }
-        return dp[level][c]=ans;
-    }
+    //     int ans=rec(level+1,c);
+    //     if(t[c]==s[level]){
+    //         ans+= rec(level+1,c+1);
+    //     }
+    //     return dp[level][c]=ans;
+    // }
 
     int numDistinct(string s, string t) {
         this->s=s;
@@ -24,6 +26,24 @@ public:
         this->n1=s.size();
         this->n2=t.size();
         memset(dp,-1,sizeof(dp));
-        return rec(0,0);
+
+        for(int level=n1;level>=0;level--){
+            for(int c=n2;c>=0;c--){
+                if(c==n2){
+                    dp[level][c]=1;
+                }
+                else if(level==n1){
+                    dp[level][c]=0;
+                }
+                else{
+                    ll ans = dp[level+1][c];
+                    if(s[level]==t[c]){
+                        ans= (ans+dp[level+1][c+1])%mod;
+                    }
+                    dp[level][c]=ans;
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
