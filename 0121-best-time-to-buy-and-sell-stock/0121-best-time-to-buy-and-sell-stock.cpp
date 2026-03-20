27288e775b95940d52dc8aa1c7c569ec33cba32c
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-03-20 18:43:34 +0530

    Time: 22 ms (6.13%), Space: 104.6 MB (5.02%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int mini =prices[0];
-        int maxprofit =0;
-        for (int i=1;i<n;i++){
-            int profit = prices[i]-mini;
-            maxprofit = max(maxprofit,profit);
-            mini=min(mini,prices[i]);
+    vector<int>* arr;
+    int dp[100001][2];
+    int n;
+
+    int rec(int level,int c){
+        if(c==2 || level==n) return 0;
+        if(dp[level][c]!=-1)return dp[level][c];
+
+        int ans =0;
+        if(c==0){
+            ans = max(ans,rec(level+1,c));
+            ans = max(ans,rec(level+1,c+1)-(*arr)[level]);
         }
-        return maxprofit;
+        else if(c==1){
+            ans = max(ans,rec(level+1,c));
+            ans = max(ans,(*arr)[level]+rec(level+1,c+1));
+        }
+        return dp[level][c]=ans;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        this->arr= &prices;
+        this->n = prices.size();
+        memset(dp,-1,sizeof(dp));
+        return rec(0,0);    
     }
 };
