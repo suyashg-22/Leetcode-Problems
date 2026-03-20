3307b6ad73106aeb055e2059dd0743283911206a
Suyash GHilahare <suyashghilahare123@gmail.com>
2026-03-20 19:42:59 +0530

    Time: 0 ms (100%), Space: 22.3 MB (26.57%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int>*arr;
+    int dp[30001][3];
+    int n;
+    int rec(int level,int c){
+        if(level==n)return 0;
+        if(dp[level][c]!=-1)return dp[level][c];
+
+        int ans =0;
+        ans=max(ans,rec(level+1,c));
+        if(c==0){
+            ans=max(ans,rec(level+1,c+1)-(*arr)[level]);
+        }
+        else{
+            ans=max(ans,rec(level+1,c-1)+(*arr)[level]);
+        }
+        return dp[level][c]=ans;
+    }
+    int maxProfit(vector<int>& prices) {
+        this->n = prices.size();
+        this->arr= & prices;
+        memset(dp,-1,sizeof(dp));
+        return rec(0,0);
+    }
+};
