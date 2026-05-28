b28e97289c1d170046776454edd136618267d094
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-05-28 13:24:29 +0530

    Time: 2 ms (72.59%), Space: 12.5 MB (12.47%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int n;
-    int dp[10];
+    int queen[10];
     vector<vector<string>>ans;
 
 
     bool check(int level,int c){
         for(int i=0;i<level;i++){
             int qi = i;
-            int qj = dp[i];
+            int qj = queen[i];
             if(c==qj || abs(qi-level)==abs(qj-c))return false;
         }
         return true;
@@ -19,7 +19,7 @@ public:
             vector<string>temp;
             for(int i=0;i<n;i++){
                 string s(n,'.');
-                s[dp[i]]='Q';
+                s[queen[i]]='Q';
                 temp.push_back(s);
             }
             ans.push_back(temp);
@@ -28,15 +28,15 @@ public:
 
         for(int c = 0;c<n;c++){
             if(check(level,c)){
-                dp[level]=c;
+                queen[level]=c;
                 rec(level+1);
-                dp[level]=-1;
+                queen[level]=-1;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         this->n=n;
-        memset(dp,-1,sizeof(dp));
+        memset(queen,-1,sizeof(queen));
         rec(0);
         return ans;
     }
