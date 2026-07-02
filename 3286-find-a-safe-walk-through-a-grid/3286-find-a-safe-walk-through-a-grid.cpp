c184800232650bcb8eba69f77a6aa157491922c8
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-07-02 19:58:02 +0530

    Time: 13 ms (76.58%), Space: 30.9 MB (70.57%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -6,18 +6,18 @@ public:
         vector<int> dx{0, 1, 0, -1};
         vector<int> dy{-1, 0, 1, 0};
 
-        priority_queue<pair<pair<int, int>, int>,
-                       vector<pair<pair<int, int>, int>>,
-                       greater<pair<pair<int, int>, int>>>
+        priority_queue<pair<int,pair<int,int>>,
+                       vector<pair<int,pair<int,int>>>,
+                       greater<pair<int,pair<int,int>>>>
             pq;
         vector<vector<int>> dis(n, vector<int>(m, 1e9));
         dis[0][0] = arr[0][0];
-        pq.push({{0, 0}, arr[0][0]});
+        pq.push({arr[0][0],{0, 0}});
         while (!pq.empty()) {
             auto it = pq.top();
-            int i = it.first.first;
-            int j = it.first.second;
-            int d = it.second;
+            int i = it.second.first;
+            int j = it.second.second;
+            int d = it.first;
             pq.pop();
             for (int z = 0; z < 4; z++) {
                 int ni = i + dx[z];
@@ -25,7 +25,7 @@ public:
                 if (ni >= 0 && ni < n && nj >= 0 && nj < m) {
                     if (d + arr[ni][nj] < dis[ni][nj]) {
                         dis[ni][nj] = d+arr[ni][nj];
-                        pq.push({{ni, nj}, d + arr[ni][nj]});
+                        pq.push({d + arr[ni][nj],{ni, nj}});
                     }
                 }
             }
