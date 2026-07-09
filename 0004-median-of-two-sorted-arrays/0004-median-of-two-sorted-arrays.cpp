c5775b5fe64781c916a8e86c57ae30e279bd7801
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-07-09 23:03:46 +0530

    Time: 0 ms (100%), Space: 94.8 MB (99.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,53 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       int n1 = nums1.size(), n2 = nums2.size();
-    //if n1 is bigger swap the arrays:
-    if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
-
-    int n = n1 + n2; //total length
-    int left = (n1 + n2 + 1) / 2; //length of left half
-    //apply binary search:
-    int low = 0, high = n1;
-    while (low <= high) {
-        int mid1 = (low + high) >> 1;
-        int mid2 = left - mid1;
-        //calculate l1, l2, r1 and r2;
-        int l1 = INT_MIN, l2 = INT_MIN;
-        int r1 = INT_MAX, r2 = INT_MAX;
-        if (mid1 < n1) r1 = nums1[mid1];
-        if (mid2 < n2) r2 = nums2[mid2];
-        if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
-        if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
-
-        if (l1 <= r2 && l2 <= r1) {
-            if (n % 2 == 1) return max(l1, l2);
-            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
-        }
-
-        //eliminate the halves:
-        else if (l1 > r2) high = mid1 - 1;
-        else low = mid1 + 1;
+    int check(int mid,vector<int>&arr,vector<int>&brr){
+        int n =arr.size();
+        int m =brr.size();
+        int size = (n+m)/2;
+        int mid2 = size-mid;
+        int l1 = (mid==0)?-1e9:arr[mid-1];
+        int l2 = (mid2==0)?-1e9:brr[mid2-1];
+        int r1 = (mid>=n)?1e9:arr[mid];
+        int r2 = (mid2>=m)?1e9:brr[mid2];
+        if(l1<=r2 && l2<=r1)return 1;
+        else if(l1<=r2)return 2;
+        return 0;
     }
-    return 0; //dummy statement
+    double bs(vector<int>&arr,vector<int>&brr){
+        int n = arr.size();
+        int m = brr.size();
+        int size = (n+m)/2;
+        int l=size-m;
+        int h=size;
+        double ans =-1;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            int mid2 = size-mid;
+
+            if(check(mid,arr,brr)==1){
+                int l1 = (mid==0)?-1e9:arr[mid-1];
+                int l2 = (mid2==0)?-1e9:brr[mid2-1];
+                int r1 = (mid>=n)?1e9:arr[mid];
+                int r2 = (mid2>=m)?1e9:brr[mid2];
+                if((n+m)%2==0){
+                    ans= (max(l1,l2)+min(r1,r2))/2.0;
+                }
+                else ans = min(r1,r2);
+                break;
+            }
+            else if(check(mid,arr,brr)==0){
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        if(n>=m)return bs(nums1,nums2);
+        else return bs(nums2,nums1);
     }
 };
