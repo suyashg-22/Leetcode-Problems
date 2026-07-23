bc5b37fccf22b83454d34e378bae7ac0971e935c
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-07-23 18:23:27 +0530

    Time: 6 ms (6.41%), Space: 8.5 MB (54.57%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,22 +1,24 @@
-#include <iostream>
-#include <climits>  // For INT_MAX and INT_MIN
-
 class Solution {
 public:
     int reverse(int x) {
-        int rev = 0;
-        
-        while (x != 0) {
-            int ld = x % 10;  // Get last digit
-            x /= 10;  // Remove last digit
-            
-            // Overflow Check
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && ld > 7)) return 0;
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && ld < -8)) return 0;
-
-            rev = rev * 10 + ld;
+        long long ans =0;
+        bool flag = (x>=0)?true:false;
+        if(!flag && x<=-2147483648)return 0;
+        long long xx= (flag)?x:-1*x;
+        long long d = xx%10;
+        xx/=10;
+        ans=d;
+        while(xx>0){
+            d=xx%10;
+            if(flag && ans>2147483647/10)return 0;
+            if(!flag && ans>2147483648/10)return 0;
+            ans*=10;
+            if(flag && ans>2147483647-d)return 0;
+            if(!flag && ans>2147483648-d)return 0;
+            ans+=d;
+            xx/=10;
         }
-
-        return rev;
+        if(flag)return ans;
+        return -1*ans;
     }
 };
