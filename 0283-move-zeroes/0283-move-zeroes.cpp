74d260b799d5ba59eea06bb8b79c18697386fe1f
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-05 23:51:58 +0530

    Time: 0 ms (100%), Space: 24 MB (19.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,19 +3,14 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int l=0;
-        int r=0;
-        while(r<n && nums[r]!=0){r++;}
-        if(r==n)return;
-        l=r;
-        r++;
+        while(l<n && nums[l]!=0)l++;
+        int r=l+1;
         while(r<n){
-            if(nums[r]==0)r++;
-            else{
-                swap(nums[l],nums[r]);
+            if(nums[r]!=0){
+                swap(nums[r],nums[l]);
                 l++;
-                r++;;
             }
+            r++;
         }
-        return;
     }
 };
