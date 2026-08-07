9e21855cef913bfc9107cd025269749110cd2d8b
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-07 11:42:47 +0530

    Time: 1 ms (35.48%), Space: 101.3 MB (5.3%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,49 +1,16 @@
 class Solution {
 public:
-    vector<int>* arr;
-    int dp[100001][3];
-    int n;
-
-    // int rec(int level,int c){
-    //     if(c==2 || level==n) return 0;
-    //     if(dp[level][c]!=-1)return dp[level][c];
-
-    //     int ans =0;
-    //     if(c==0){
-    //         ans = max(ans,rec(level+1,c));
-    //         ans = max(ans,rec(level+1,c+1)-(*arr)[level]);
-    //     }
-    //     else if(c==1){
-    //         ans = max(ans,rec(level+1,c));
-    //         ans = max(ans,(*arr)[level]+rec(level+1,c+1));
-    //     }
-    //     return dp[level][c]=ans;
-    // }
-
     int maxProfit(vector<int>& prices) {
-        this->arr= &prices;
-        this->n = prices.size();
-        memset(dp,-1,sizeof(dp));
-
-        for(int level=n;level>=0;level--){
-            for(int c=2;c>=0;c--){
-                if(level==n || c==2){
-                    dp[level][c]=0;
-                }
-                else {
-                    int ans =0;
-                    if(c==0){
-                        ans=max(ans,dp[level+1][c]);
-                        ans=max(ans,dp[level+1][c+1]-(*arr)[level]);
-                    }
-                    else if(c==1){
-                        ans=max(ans,dp[level+1][c]);
-                        ans=max(ans,dp[level+1][c+1]+(*arr)[level]);
-                    }
-                    dp[level][c]=ans;
-                }
-            }
+        int n = prices.size();
+        vector<int>arr(n);
+        arr[n-1]=prices[n-1];
+        for(int i=n-2;i>=0;i--){
+            arr[i]=max(arr[i+1],prices[i]);
         }
-        return dp[0][0];    
+        int maxi=0;
+        for(int i=0;i<n-1;i++){
+            maxi=max(maxi,-prices[i]+arr[i+1]);
+        }
+        return maxi;
     }
 };
