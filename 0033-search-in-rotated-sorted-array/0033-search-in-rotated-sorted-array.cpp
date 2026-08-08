ceaf45e2a2f760fb4bf320cd5e799a5cace71684
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-09 01:02:19 +0530

    Time: 0 ms (100%), Space: 15.2 MB (69.26%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
-    vector<int>*arr;
-    int target;
-    int n;
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
 
-    bool check(int l,int h,int mid){
-
-        if((*arr)[l]<= (*arr)[mid]){
-            if(target>= (*arr)[l] && target<=(*arr)[mid])return 0;
-            return 1;
-        }
-        else{
-            if(target>=(*arr)[mid+1] && target <= (*arr)[h])return 1;
-            return 0;
-        }
-    }
-
-    int bs(){
         int l =0;
-        int h =n-1;
+        int h=n-1;
         while(l<=h){
-            int mid = l+(h-l)/2;
-            if((*arr)[mid]==target)return mid;
-
-            if(check(l,h,mid)){
-                l=mid+1;
+            int mid= l+(h-l)/2;
+            if(nums[mid]==target)return mid;
+            if(nums[l]<=nums[mid]){
+                if(target<=nums[mid] && target>=nums[l])h=mid-1;
+                else l=mid+1;
             }
-            else h=mid-1;
+            else{
+                if(nums[mid]<=target && target<=nums[h])l=mid+1;
+                else h=mid-1;
+            }
         }
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        this->arr=&nums;
-        this->target= target;
-        this->n=nums.size();
-        return bs();
     }
 };
