6e2fcd08126d3b9a92e9206f420c8f2cedd19530
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-09 17:33:05 +0530

    Time: 0 ms (100%), Space: 10.1 MB (90.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int dp[102];
+    int dp[101];
+    int rec(int level,vector<int>&arr,int n){
+        if(level>=n)return 0;
+        if(dp[level]!=-1)return dp[level];
+        int ans= rec(level+1,arr,n);
+        ans= max(ans,arr[level]+rec(level+2,arr,n));
+        return dp[level]=ans;
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-
-        for(int level=n+1;level>=0;level--){
-            if(level==n || level==n+1)dp[level]=0;
-            else{
-                int ans = dp[level+1];
-                ans=max(ans,nums[level]+dp[level+2]);
-                dp[level]=ans;
-            }
-        }
-        return dp[0];
+        memset(dp,-1,sizeof(dp));
+        return rec(0,nums,n);
     }
 };
