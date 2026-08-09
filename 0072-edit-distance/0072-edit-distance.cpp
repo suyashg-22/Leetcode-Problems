b4b14f90ce5ad95402e2af707f0d459778e74155
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-09 23:46:23 +0530

    Time: 54 ms (10.51%), Space: 10.6 MB (88.52%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    string s1, s2;
-    int n1, n2;
     int dp[501][501];
-
-    int rec(int i,int j){
-        if(i==n1 && j==n2)return 0;
-        if(i==n1)return n2-j;
-        if(j==n2)return n1-i;
+    int rec(int i,int j,string& s1,string &s2,int n,int m){
+        if(i==n && j==m)return 0;
+        if(i==n)return m-j;
+        if(j==m)return n-i;
         if(dp[i][j]!=-1)return dp[i][j];
-
-        int ans = 1e8;
+        int ans = 1e9;
         if(s1[i]==s2[j]){
-            ans = rec(i+1,j+1);
+            ans= rec(i+1,j+1,s1,s2,n,m);
         }
         else{
-            ans=1+rec(i,j+1);
-            ans=min(ans,1+rec(i+1,j));
-            ans=min(ans,1+rec(i+1,j+1));
+            ans= min(ans,1+rec(i,j+1,s1,s2,n,m));
+            ans= min(ans,1+rec(i+1,j,s1,s2,n,m));
+            ans= min(ans,1+rec(i+1,j+1,s1,s2,n,m));
         }
         return dp[i][j]=ans;
     }
-
     int minDistance(string word1, string word2) {
-        this->s1 = word1;
-        this->s2 = word2;
-
-        n1 = s1.size();
-        n2 = s2.size();
-
-        memset(dp, -1, sizeof(dp));
-
-        return rec(0,0);
+        int n = word1.size();
+        int m = word2.size();
+        memset(dp,-1,sizeof(dp));
+        return rec(0,0,word1,word2,n,m);
     }
 };
