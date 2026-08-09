510c277f821227dae2d7d65271f122492815ea9b
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-10 00:00:11 +0530

    Time: 7 ms (40.03%), Space: 22.4 MB (40.08%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    vector<int>*arr;
-    int dp[30001][3];
-    int n;
-    // int rec(int level,int c){
-    //     if(level==n)return 0;
-    //     if(dp[level][c]!=-1)return dp[level][c];
-
-    //     int ans =0;
-    //     ans=max(ans,rec(level+1,c));
-    //     if(c==0){
-    //         ans=max(ans,rec(level+1,c+1)-(*arr)[level]);
-    //     }
-    //     else{
-    //         ans=max(ans,rec(level+1,c-1)+(*arr)[level]);
-    //     }
-    //     return dp[level][c]=ans;
-    // }
-    int maxProfit(vector<int>& prices) {
-        this->n = prices.size();
-        this->arr= & prices;
-        memset(dp,-1,sizeof(dp));
-        for(int level=n;level>=0;level--){
-            for(int c=0;c<2;c++){
-                if(level==n){
-                    dp[level][c]=0;
-                }
-                else{
-                    int ans =0;
-                    ans = max(ans,dp[level+1][c]);
-                    if(c==0){
-                        ans = max(ans,dp[level+1][c+1]-(*arr)[level]);                        
-                    }
-                    else{
-                        ans=max(ans,dp[level+1][c-1]+(*arr)[level]);
-                    }
-                    dp[level][c]=ans;
-                }
-            }
+    int dp[30001][2];
+    int rec(int level,int c,vector<int>&arr,int n){
+        if(level==n)return 0;
+        if(dp[level][c]!=-1)return dp[level][c];
+        int ans =0;
+        if(c==0){
+            ans=max(ans,rec(level+1,c,arr,n));
+            ans=max(ans,rec(level+1,1,arr,n)-arr[level]);
         }
-        return dp[0][0];
+        else{
+            ans=max(ans,rec(level+1,c,arr,n));
+            ans=max(ans,rec(level+1,0,arr,n)+arr[level]);
+        }
+        return dp[level][c]=ans;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        memset(dp,-1,sizeof(dp));
+        return rec(0,0,prices,n);
     }
 };
