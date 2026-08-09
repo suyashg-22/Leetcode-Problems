3cdc007720c84a01b9948053bfc03f54f276b72c
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-09 17:49:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (89.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
+    int dp[101][101];
+    int rec(int i,int j,int n,int m){
+        if(i==n-1 && j==m-1)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=0;
+        if(j+1<m)ans+= rec(i,j+1,n,m);
+        if(i+1<n)ans+= rec(i+1,j,n,m);
+        return dp[i][j]=ans;
+    }
     int uniquePaths(int m, int n) {
-        int dp[101][101];
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(i==m-1 && j==n-1){
-                    dp[i][j]=1;
-                }
-                else{
-                    int ans =0;
-                    if(i+1<m)ans+=dp[i+1][j];
-                    if(j+1<n)ans+=dp[i][j+1];
-                    dp[i][j]=ans;
-                }
-            }
-        }
-        return dp[0][0];
+        memset(dp,-1,sizeof(dp));
+        return rec(0,0,m,n);
     }
 };
