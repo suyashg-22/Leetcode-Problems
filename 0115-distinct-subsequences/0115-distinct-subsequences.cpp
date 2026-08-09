d35301e8a89fa18cf36e6aadb4b96871bf4a1f3c
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-09 23:24:38 +0530

    Time: 28 ms (62.59%), Space: 13.2 MB (78.63%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,49 +1,20 @@
-using ll = long long;
-const ll mod = 1e9+9;
 class Solution {
 public:
-    string s;
-    string t;
-    int n1;
-    int n2;
-    ll dp[1001][1001];
-
-    // int rec(int level,int c){
-    //     if(c==n2)return 1;
-    //     if(level==n1)return 0;
-    //     if(dp[level][c]!=-1)return dp[level][c];
-
-    //     int ans=rec(level+1,c);
-    //     if(t[c]==s[level]){
-    //         ans+= rec(level+1,c+1);
-    //     }
-    //     return dp[level][c]=ans;
-    // }
-
-    int numDistinct(string s, string t) {
-        this->s=s;
-        this->t=t;
-        this->n1=s.size();
-        this->n2=t.size();
-        memset(dp,-1,sizeof(dp));
-
-        for(int level=n1;level>=0;level--){
-            for(int c=n2;c>=0;c--){
-                if(c==n2){
-                    dp[level][c]=1;
-                }
-                else if(level==n1){
-                    dp[level][c]=0;
-                }
-                else{
-                    ll ans = dp[level+1][c];
-                    if(s[level]==t[c]){
-                        ans= (ans+dp[level+1][c+1])%mod;
-                    }
-                    dp[level][c]=ans;
-                }
-            }
+    int dp[1001][1001];
+    int rec(int i,int j,string &s,string&t,int n,int m){
+        if(j==m)return 1;
+        if(i==n)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans = rec(i+1,j,s,t,n,m);
+        if(s[i]==t[j]){
+            ans+= rec(i+1,j+1,s,t,n,m);
         }
-        return dp[0][0];
+        return dp[i][j]=ans;
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        memset(dp,-1,sizeof(dp));
+        return rec(0,0,s,t,n,m);
     }
 };
