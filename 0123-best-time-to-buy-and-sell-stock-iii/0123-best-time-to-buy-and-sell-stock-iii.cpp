9262ed2c4d3274e3fdaaf00791a42b0b887413f0
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-11 17:38:56 +0530

    Time: 39 ms (74.93%), Space: 89.3 MB (66.54%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,24 @@
-using ll = long long;
 class Solution {
 public:
-    vector<int>*arr;
-    ll n;
-    ll dp[100001][5];
-    ll rec(ll level,ll c){
-        if(c==4)return 0;
-        if(level==n){
-            return 0;
-        }
-        if(dp[level][c]!=-1)return dp[level][c];
-        ll ans =0;
-        if(c%2==0){
-            ans=max(ans,rec(level+1,c));
-            ans=max(ans,rec(level+1,c+1)-(*arr)[level]);
+    int dp[100001][5];
+    int rec(int level,int t,vector<int>&arr,int n){
+        if(level==n)return 0;
+        if(t==4)return 0;
+        if(dp[level][t]!=-1)return dp[level][t];
+        int maxi =0;
+        if(t%2==0){
+            maxi=max(maxi,rec(level+1,t,arr,n));
+            maxi=max(maxi,-arr[level]+rec(level+1,t+1,arr,n));
         }
         else{
-            ans=max(ans,rec(level+1,c));
-            ans=max(ans,rec(level+1,c+1)+(*arr)[level]);
+            maxi=max(maxi,rec(level+1,t,arr,n));
+            maxi=max(maxi,arr[level]+rec(level+1,t+1,arr,n));
         }
-        return dp[level][c]=ans;
+        return dp[level][t]=maxi;
     }
-    ll maxProfit(vector<int>& prices) {
-        this->arr=&prices;
-        this->n = prices.size();
+    int maxProfit(vector<int>& prices) {
+        int n =prices.size();
         memset(dp,-1,sizeof(dp));
-        return rec(0,0);
+        return rec(0,0,prices,n);
     }
 };
