aaaf65deeb5432f99408186b083f3c745318da6d
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-11 18:47:23 +0530

    Time: 11 ms (62.98%), Space: 12.4 MB (99.26%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,51 +1,44 @@
 class Solution {
 public:
-    vector<int>*arr;
-    int n;
     int dp[1001];
-    vector<int>hash;
-
-    int rec(int level){
+    int rec(int level,vector<int>&arr,vector<int>&hash,int n){
         if(level<0)return 0;
         if(dp[level]!=-1)return dp[level];
 
-        int ans =1;
-        for(int l = 0;l<level;l++){
-            if((*arr)[level]%(*arr)[l]==0){
-                int ans2 = 1+rec(l);
-                if(ans2>ans){
-                    hash[level]=l;
-                    ans=ans2;
+        int ans=1;
+        for(int i=level-1;i>=0;i--){
+            if(arr[level]%arr[i]==0){
+                int temp= 1+rec(i,arr,hash,n);
+                if(temp>ans){
+                    hash[level]=i;
+                    ans=temp;
                 }
             }
         }
         return dp[level]=ans;
     }
-    
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        this->n=nums.size();
-        this->arr=&nums;
+        int n =nums.size();
         memset(dp,-1,sizeof(dp));
-        hash.resize(n);
-        for(int i=0;i<n;i++)hash[i]=i;
         sort(nums.begin(),nums.end());
-
-        vector<int>ans;
-        int maxi=1;
-        int maxiind=0;
-        for(int i=n-1;i>=0;i--){
-            int ans2 = rec(i);
-            if(ans2>maxi){
-                maxi=ans2;
+        vector<int>hash(n);
+        for(int i=0;i<n;i++)hash[i]=i;
+        int maxi=-1;
+        int maxiind=-1;
+        for(int i=0;i<n;i++){
+            int temp = rec(i,nums,hash,n);
+            if(temp>maxi){
+                maxi=temp;
                 maxiind=i;
             }
         }
-        int i = maxiind;
-        while(hash[i]!=i){
-            ans.push_back(nums[i]);
-            i=hash[i];
+        vector<int>ans;
+        while(hash[maxiind]!=maxiind){
+            ans.push_back(nums[maxiind]);
+            maxiind=hash[maxiind];
         }
-        ans.push_back(nums[i]);
+        ans.push_back(nums[maxiind]);
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
