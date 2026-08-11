70b221ba9f2f2339b8d632a937b511d4e43d270a
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-11 20:23:36 +0530

    Time: 42 ms (17.75%), Space: 17.4 MB (91.68%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-    int n;
-    vector<int>*arr;
     int dp[2001];
-    vector<int>hash;
-
-    int rec(int level){
-        if(level<0)return 0;
-        if(dp[level]!=-1)return dp[level];
-
+    int rec(int level, vector<int>& arr,vector<int>&hash) {
+        if (level < 0) return 0;
+        if (dp[level] != -1) return dp[level];
         int ans = 1;
-        for(int l=0;l<level;l++){
-            if((*arr)[l]<(*arr)[level]){
-                int ans2=1+rec(l);
-                if(ans2==ans){
-                    hash[level]+=hash[l];
-                }
-                else if (ans2>ans){
+        int cnt = 1;
+        for (int i = level - 1; i >= 0; i--) {
+            if (arr[i] < arr[level]){
+                int ans2 = 1+rec(i,arr,hash);
+                if(ans2>ans){
                     ans=ans2;
-                    hash[level]=hash[l];
+                    cnt=hash[i];
                 }
-
+                else if(ans2==ans){
+                    cnt+=hash[i];
+                }
             }
         }
-        return dp[level]=ans;
+        hash[level]=cnt;
+        return dp[level] = ans;
     }
-    int findNumberOfLIS(vector<int>& nums) {
-        this->n = nums.size();
-        this->arr=&nums;
-        memset(dp,-1,sizeof(dp));
-        hash.assign(n+1,1);
 
-        int maxi=1;
-        int cnt=0;
+    int findNumberOfLIS(vector<int>& nums) {
+        int n =nums.size();
+        memset(dp,-1,sizeof(dp));
+        vector<int>hash(n,1);
+
+        int maxi=-1;
         for(int i=0;i<n;i++){
-            maxi=max(maxi,rec(i));
+            maxi=max(maxi,rec(i,nums,hash));
         }
+        int cnt =0;
         for(int i=0;i<n;i++){
-            if(dp[i]==maxi)cnt+=hash[i];
+            if(rec(i,nums,hash)==maxi)cnt+=hash[i];
         }
-        return cnt;
+        return cnt;;
     }
 };
