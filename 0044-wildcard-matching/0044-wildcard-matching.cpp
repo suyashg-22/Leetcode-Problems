71e3b25d019dc078636eaa1dc55c59d5bcfb6936
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-11 17:28:15 +0530

    Time: 1433 ms (5.01%), Space: 27 MB (46.51%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    int n1;
-    int n2;
-    string s,p;
     int dp[2001][2001];
-    
-    bool rec(int i,int j){
-        if(i==n1 && j==n2)return 1;
-        if(j==n2)return 0;
-        if(i==n1 && p[j]!='*')return 0;
+    bool rec(int i,int j,string &s,string&p,int n,int m){
+        if(i==n && j==m)return true;
+        if(j==m)return false;
+        if(i==n && p[j]!='*')return false;
 
         if(dp[i][j]!=-1)return dp[i][j];
+        bool ans =false;
+        if(p[j]=='?'){
+            ans= ans| rec(i+1,j+1,s,p,n,m);
+        }
+        else if(p[j]=='*'){
+            if(i<n)ans=ans|rec(i+1,j,s,p,n,m);
+            ans=ans|rec(i,j+1,s,p,n,m);
+        }
+        else if(s[i]==p[j])ans=ans|rec(i+1,j+1,s,p,n,m);
 
-        bool ans = 0;
-        if(p[j]=='*'){
-            if(i<n1)ans = rec(i+1,j);
-            ans = (ans | rec(i,j+1));
-        }
-        else if(p[j]=='?'){
-            ans = rec(i+1,j+1);
-        }
-        else{
-            if(s[i]==p[j]){
-                ans = rec(i+1,j+1);
-            }
-            else{
-                ans = 0;
-            }
-        }
         return dp[i][j]=ans;
     }
     bool isMatch(string s, string p) {
-        this->s=s;
-        this->p=p;
-        this->n1=s.size();
-        this->n2=p.size();
+        int n = s.size();
+        int m = p.size();
         memset(dp,-1,sizeof(dp));
-        return rec(0,0);
+        return rec(0,0,s,p,n,m);
     }
 };
