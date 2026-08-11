faf79acfb36e77f584ca99b1bf815a03300c0775
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-11 17:55:36 +0530

    Time: 16 ms (70.83%), Space: 67.9 MB (68.64%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int fee;
-    int n;
-    vector<int>*arr;
     int dp[50001][2];
-    int rec(int level,int c){
-        if(level==n)return 0;
-        if(dp[level][c]!=-1)return dp[level][c];
-
-        int ans =0;
-        ans = max(ans,rec(level+1,c));
-        if(c==0){
-            ans=max(ans,rec(level+1,1)-(*arr)[level]);
+    int rec(int level,int t,vector<int>&arr,int n,int f){
+        if(level>=n)return 0;
+        if(dp[level][t]!=-1)return dp[level][t];
+        int ans=0;
+        ans=max(ans,rec(level+1,t,arr,n,f));
+        if(t==0){
+            ans=max(ans,-arr[level]+rec(level+1,1,arr,n,f));
         }
-        else{
-            ans=max(ans,rec(level+1,0)+(*arr)[level]-fee);
+        else {
+            ans=max(ans,-f+arr[level]+rec(level+1,0,arr,n,f));
         }
-        return dp[level][c]=ans;
+        return dp[level][t]=ans;
     }
     int maxProfit(vector<int>& prices, int fee) {
-        this->arr=&prices;
-        this->n=prices.size();
-        this->fee=fee;
+        int n=prices.size();
         memset(dp,-1,sizeof(dp));
-        return rec(0,0);
+        return rec(0,0,prices,n,fee);
     }
 };
