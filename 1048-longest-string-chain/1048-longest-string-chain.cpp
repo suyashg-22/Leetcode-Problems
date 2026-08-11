435e84cdab40af62d80b0c54be483d5a20616e0b
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-11 19:52:33 +0530

    Time: 38 ms (78.74%), Space: 17.6 MB (99.78%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,53 +1,48 @@
 class Solution {
 public:
-    vector<string>*arr;
-    int n;
     int dp[1001];
-    int rec(int level){
-        if(level<0)return 0;
-        if(dp[level]!=-1)return dp[level];
-
-        int s2 = (*arr)[level].size();
-        int ans = 1;
-        for(int l=level-1;l>=0;l--){
-            int s1 = (*arr)[l].size();
-            if(s1+1 == s2){
-                int i=0;
-                int j=0;
-                string a = (*arr)[l];
-                string b = (*arr)[level];
-                while(j<s2){
-                    if(i>=s1)break;
-                    if(a[i]==b[j]){
-                        i+=1;
-                        j+=1;
-                    }
-                    else j+=1;
-                }
-
-                if(i>=s1){
-                    ans = max(ans, 1+rec(l));
-                }
+    bool fun(string& s1,string& s2){
+        int i=0;
+        int j=0;
+        int n=s1.size();
+        int m=s2.size();
+        if(m!=n+1)return false;
+        while(j<m){
+            if(s1[i]!=s2[j]){
+                j++;
             }
-
+            else {
+                i++;
+                j++;
+            }
         }
-        return dp[level]=ans;
+        if(i==n && j==m)return true;
+        return false;
     }
-    static bool comp(const string &x ,const string &y){
-        // if(x.size()<y.size())return 1;
-        // else return 0;
-        return x.size()<y.size();
+    int rec(int level, vector<string>& arr, int n) {
+        if (level < 0) return 0;
+        if (dp[level] != -1) return dp[level];
+
+        int ans = 1;
+        for (int i = 0; i < level; i++) {
+            if (fun(arr[i], arr[level])) { 
+                ans = max(ans, 1 + rec(i, arr, n));
+            }
+        }
+        return dp[level] = ans;
     }
 
+    static bool comp(const string &s1,const string&s2){
+        return s1.size()<s2.size();
+    }
     int longestStrChain(vector<string>& words) {
-        this->n = words.size();
-        this->arr= &words;
+        int n =words.size();
         memset(dp,-1,sizeof(dp));
         sort(words.begin(),words.end(),comp);
-        int maxi = 1;
-        for(int i=n-1;i>=0;i--){
-            maxi=max(maxi,rec(i));
+        int ans =0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,rec(i,words,n));
         }
-        return maxi;
+        return ans;
     }
 };
