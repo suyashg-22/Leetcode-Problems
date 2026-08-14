2931c262c6b3e4738c208e10beb053efc44b7052
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-15 00:42:57 +0530

    Time: 3 ms (72.49%), Space: 27.4 MB (54.24%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,28 +10,22 @@
  * };
  */
 class Solution {
-public: 
-    TreeNode* f(vector<int>& preorder,int prestart,int preend, vector<int>& inorder,int instart,int inend,unordered_map<int,int>&mpp){
-        if(prestart>preend || instart>inend) return NULL;
-
-        TreeNode* root = new TreeNode(preorder[prestart]);
-        int inroot = mpp[root->val];
-        int numsleft = inroot-instart;
-
-        root->left = f(preorder,prestart+1,prestart+numsleft,inorder,instart,inroot-1,mpp);
-        root->right = f(preorder,prestart+numsleft+1,preend,inorder,inroot+1,inend,mpp);
+public:
+    TreeNode* build(vector<int>&pre,int prestart,int preend,vector<int>&ino,int inostart,int inoend,unordered_map<int,int>&mpp){
+        if(prestart>preend||inostart>inoend)return NULL;
+        
+        TreeNode* root = new TreeNode(pre[prestart]);
+        int inoroot = mpp[pre[prestart]];
+        int inoleft = inoroot-inostart;
+        root->left = build(pre,prestart+1,prestart+inoleft,ino,inostart,inoroot-1,mpp);
+        root->right = build(pre,prestart+inoleft+1,preend,ino,inoroot+1,inoend,mpp);
         return root;
-
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         unordered_map<int,int>mpp;
-        int n = preorder.size();
-        int m = inorder.size();
-        for(int i=0;i<m;i++){
-            mpp[inorder[i]]=i;
-        }
-
-        TreeNode* root = f(preorder,0,n-1,inorder,0,m-1,mpp);
-        return root;
+        int n= preorder.size();
+        int m= inorder.size();
+        for(int i=0;i<m;i++)mpp[inorder[i]]=i;
+        return build(preorder,0,n-1,inorder,0,m-1,mpp);
     }
 };
