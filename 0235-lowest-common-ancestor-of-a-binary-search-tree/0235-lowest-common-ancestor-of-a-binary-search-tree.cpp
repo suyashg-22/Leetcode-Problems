47d0f0d97c8ceaa812f5cd4398b909192e179970
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-15 19:09:57 +0530

    Time: 22 ms (65.18%), Space: 23.6 MB (36.54%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -8,34 +8,19 @@
  * };
  */
 
-class Solution
-{
+class Solution {
 public:
-    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
-    {
-        TreeNode *node = root;
-        TreeNode *ans = NULL;
-        while (node)
-        {
-            if (node == p || node == q)
-            {
-                ans=node;
-                break;
-            }
-            else if ((p->val < node->val && node->val < q->val) || (q->val < node->val && node->val < p->val))
-            {
-                ans=node;
-                break;
-            }
-            else{
-                if(node->val>p->val && node->val>q->val){
-                    node=node->left;
-                }
-                else{
-                    node=node->right;
-                }
-            }
-        }
-        return ans;
+    TreeNode* rec(TreeNode* node,TreeNode* p,TreeNode* q){
+        if(!node)return NULL;
+        if(node==p || node==q)return node;
+        int mini = min(p->val,q->val);
+        int maxi = max(p->val,q->val);
+        int x = node->val;
+        if(mini<x && x<maxi)return node;
+        else if(maxi<x) return rec(node->left,p,q);
+        return rec(node->right,p,q);
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return rec(root,p,q);
     }
 };
