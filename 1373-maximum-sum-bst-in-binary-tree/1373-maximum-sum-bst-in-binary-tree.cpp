8f2f3af369cd6022cac3b8378cda27b643dd7537
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-16 18:51:46 +0530

    Time: 9 ms (41.15%), Space: 104.6 MB (67.48%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -1,38 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct node{
+    bool flag;
+    int sum;
+    int mini;
+    int maxi;
+};
+
 class Solution {
 public:
-    int ans = 0;
 
-    struct node {
-        bool bst;
-        int sum;
-        int mn;
-        int mx;
-    };
-
-    node dfs(TreeNode* u) {
-        if (!u)
-            return {true, 0, INT_MAX, INT_MIN};
-
-        auto l = dfs(u->left);
-        auto r = dfs(u->right);
-
-        if (l.bst && r.bst && l.mx < u->val && u->val < r.mn) {
-            int s = l.sum + r.sum + u->val;
-            ans = max(ans, s);
-
-            return {
-                true,
-                s,
-                min(l.mn, u->val),
-                max(r.mx, u->val)
-            };
+    node rec(TreeNode* cur,int &ans){
+        if(!cur)return{true,0,INT_MAX,INT_MIN};
+        node l =rec(cur->left,ans);
+        node r =rec(cur->right,ans);
+        if(l.flag && r.flag && l.maxi<cur->val && cur->val<r.mini){
+            int s = l.sum+r.sum+cur->val;
+            ans=max(ans,s);
+            return {true,s,min(l.mini,cur->val),max(r.maxi,cur->val)};
         }
-
-        return {false, 0, 0, 0};
+        return {false,0,0,0};
     }
 
     int maxSumBST(TreeNode* root) {
-        dfs(root);
+        int ans= 0;
+        rec(root,ans);
         return ans;
     }
 };
