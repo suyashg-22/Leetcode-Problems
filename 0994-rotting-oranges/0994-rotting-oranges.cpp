9df1f2a3fc88236ee09c078b0391b298092436e0
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-19 23:11:48 +0530

    Time: 0 ms (100%), Space: 17.3 MB (10.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,70 +1,48 @@
-class Solution
-{
+class Solution {
 public:
-    queue<pair<int, int>> q;
-    int m, n;
-    vector<vector<int>> *arr;
-    map<pair<int, int>, bool> vis;
-    int cnt = 0;
-    vector<pair<int, int>> padosi{{-1, 0}, {+1, 0}, {0, -1}, {0, +1}};
-    int orange1 = 0;
-
-    void bfs()
-    {
-        while (!q.empty())
-        {
-            int size = q.size();
-            bool rotted_this_round = false; 
-            for (int s = 0; s < size; s++)
-            {
-                auto p = q.front();
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int>dx{0,1,0,-1};
+        vector<int>dy{-1,0,1,0};
+        queue<pair<int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                    vis[i][j]=1;
+                }
+            }
+        }
+        int t=0;
+        while(!q.empty()){
+            int size=q.size();
+            bool flag = false;
+            for(int i=0;i<size;i++){
+                auto it = q.front();
                 q.pop();
-                vis[p] = 1;
-                int x = p.first;
-                int y = p.second;
-
-                for (int i = 0; i < padosi.size(); i++)
-                {
-                    pair<int, int> z = {x + padosi[i].first, y + padosi[i].second};
-                    if (z.first >= 0 && z.first < m && z.second >= 0 && z.second < n)
-                    {
-                        if ((*arr)[z.first][z.second] == 1)
-                        {
-                            if (vis.find(z) == vis.end())
-                            {
-                                q.push(z);
-                                vis[z] = 1;
-                                (*arr)[z.first][z.second] = 2; 
-                                orange1 -= 1;
-                                rotted_this_round = true;
-                            }
+                int x = it.first;
+                int y = it.second;
+                for(int z=0;z<4;z++){
+                    int nx=x+dx[z];
+                    int ny=y+dy[z];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m){
+                        if(grid[nx][ny]==1 && vis[nx][ny]==0){
+                            vis[nx][ny]=1;
+                            q.push({nx,ny});
+                            flag=true;
                         }
                     }
                 }
             }
-            if (rotted_this_round) cnt += 1; 
+            if(flag)t++;
         }
-    }
-
-    int orangesRotting(vector<vector<int>> &grid)
-    {
-        m = grid.size();
-        n = grid[0].size();
-        arr = &grid;
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (grid[i][j] == 2)
-                    q.push({i, j});
-                if (grid[i][j] == 1)
-                    orange1 += 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && vis[i][j]==0)return -1;
             }
         }
-
-        bfs();
-
-        if (orange1 != 0) return -1;
-        return cnt; 
+        return t;
     }
 };
