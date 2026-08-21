6f608df13145d10449845460d554dd36311316ec
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-21 18:37:18 +0530

    Time: 113 ms (5.1%), Space: 62.9 MB (5.18%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,69 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-const long long mod = 1000000009;
-
-#define asc(x) (x).begin(), (x).end()
-#define desc(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define ff first
-#define ss second
-#define forl(i, a, b) for (ll i = (a); i < (b); i++)
-#define fore(x, a) for (auto& x : a)
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
 class Solution {
 public:
-    int m, n;
-    queue<pair<pair<int, int>, int>> q;
-    vector<vector<int>>* arr;
-    vector<pair<int, int>> padosi{{-1, 0}, {+1, 0}, {0, -1}, {0, +1}};
-    vector<vector<int>>vis;
-
-    void bfs() {
-        while (!q.empty()) {
-            int size = q.size();
-            auto p = q.front();
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n =mat.size();
+        int m= mat[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,-1));
+        vector<vector<int>>dis(n,vector<int>(m,-1));
+        queue<vector<int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({i,j,0});
+                    vis[i][j]=1;
+                    dis[i][j]=0;
+                }
+            }
+        }
+        vector<int>dx{0,1,0,-1};
+        vector<int>dy{-1,0,1,0};
+        while(!q.empty()){
+            auto it =q.front();
+            int x=it[0];
+            int y=it[1];
+            int d=it[2];
             q.pop();
-            int x = p.ff.ff;
-            int y = p.ff.ss;
-            int c = p.ss;
-            vis[x][y]=1;
-
-            for (int i = 0; i < padosi.size(); i++) {
-                pair<pair<int, int>, int> z = {
-                    {x + padosi[i].first, y + padosi[i].second}, c + 1};
-                int nx = z.ff.ff;
-                int ny = z.ff.ss;
-                int nc = z.ss;
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                    if (vis[nx][ny]==0) {
-                        q.push(z);
-                        (*arr)[nx][ny] = nc;
+            for(int z=0;z<4;z++){
+                int nx=x+dx[z];
+                int ny=y+dy[z];
+                if(nx>=0 && nx<n && ny>=0 && ny<m){
+                    if(vis[nx][ny]==-1 && mat[nx][ny]==1){
+                        q.push({nx,ny,d+1});
+                        dis[nx][ny]=d+1;
                         vis[nx][ny]=1;
                     }
                 }
             }
         }
-    }
-
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        m = mat.size();
-        n = mat[0].size();
-        arr = &mat;
-        vis.assign(m,vector<int>(n,0));
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    q.push({{i, j}, 0});
-                    vis[i][j]=1;
-                }
-            }
-        }
-
-        bfs();
-        return mat;
+        return dis;
     }
 };
