7d8f75ec48583b0bd7211e6cb106069e76ffc9f3
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-21 19:05:37 +0530

    Time: 7 ms (73.97%), Space: 33.1 MB (61.5%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,67 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-const long long mod = 1000000009;
-
-#define asc(x) (x).begin(), (x).end()
-#define desc(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define ff first
-#define ss second
-#define forl(i, a, b) for (ll i = (a); i < (b); i++)
-#define fore(x, a) for (auto& x : a)
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
 class Solution {
 public:
-    int n, m;
-    vector<vector<int>>* arr;
-    vector<vector<bool>> vis;
-    queue<pair<int, int>> q;
-    int cnt =0;
-    void bfs() {
-        vector<pair<int, int>> padosi{{-1, 0}, {+1, 0}, {0, -1}, {0, +1}};
-
-        while (!q.empty()) {
-            auto p = q.front();
-            int x = p.ff;
-            int y = p.ss;
-            vis[x][y] = 1;
-            q.pop();
-
-            for (auto it : padosi) {
-                int nx = x + it.ff;
-                int ny = y + it.ss;
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                    if (!vis[nx][ny] && (*arr)[nx][ny] == 1) {
-                        vis[nx][ny] = 1;
-                        q.push({nx, ny});
-                        cnt-=1;
-                    }
+    vector<int>dx{0,1,0,-1};
+    vector<int>dy{-1,0,1,0};
+    void dfs(int x,int y,int n,int m,vector<vector<int>>&arr){
+        arr[x][y]=0;
+        for(int z=0;z<4;z++){
+            int nx=x+dx[z];
+            int ny=y+dy[z];
+            if(nx<n && nx>=0 && ny>=0 && ny<m){
+                if(arr[nx][ny]==1){
+                    dfs(nx,ny,n,m,arr);
                 }
             }
         }
+        return;
     }
-
     int numEnclaves(vector<vector<int>>& grid) {
-        n=grid[0].size();
-        m=grid.size();
-        arr=&grid;
-        vis.assign(m,vector<bool>(n,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || i==m-1 || j==0 || j==n-1){
-                    if(grid[i][j]==1){
-                        q.push({i,j});
-                        vis[i][j]=1;
-                    }
-                }
-                else if(grid[i][j]==1){
-                    cnt+=1;
+        int n =grid.size();
+        int m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || i==n-1 || j==0 ||j==m-1){
+                    if(grid[i][j]==1)dfs(i,j,n,m,grid);
                 }
             }
         }
-        bfs();
-        return cnt;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)ans++;
+            }
+        }
+        return ans;
     }
 };
