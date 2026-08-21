11fb8544a563b966e661acea8d29384f9342c6a3
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-22 00:03:31 +0530

    Time: 8 ms (34.89%), Space: 20 MB (39.2%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,31 @@
-class Solution
-{
+class Solution {
 public:
-    int n;
-    vector<vector<int>> adj;
-    vector<int> inor;
-    vector<int> ans;
-
-    void bfs()
-    {
-        queue<int> q;
-        for (int i = 0; i < n; i++)
-        {
-            if (inor[i] == 0)
-            {
-                q.push(i);
-            }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n =numCourses;
+        int cnt=0;
+        queue<int>q;
+        vector<int>indeg(n,0);
+        vector<vector<int>>adj(n);
+        for(auto it:prerequisites){
+            int a = it[0];
+            int b = it[1];
+            adj[b].push_back(a);
+            indeg[a]+=1;
         }
-
-        while (!q.empty())
-        {
-            int p = q.front();
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0)q.push(i);   
+        }
+        while(!q.empty()){
+            int node =q.front();
             q.pop();
-            ans.push_back(p);
-            for (auto x : adj[p])
-            {
-                inor[x] -= 1;
-                if (inor[x] == 0)
-                    q.push(x);
+            cnt++;
+            for(auto it:adj[node]){
+                if(indeg[it]>0){
+                    indeg[it]--;
+                    if(indeg[it]==0)q.push(it);
+                }
             }
         }
-    }
-    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
-    {
-        this->n = numCourses;
-        this->adj.resize(n);
-        this->inor.assign(n, 0);
-        ans.clear();
-        for (int i = 0; i < prerequisites.size(); i++)
-        {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            inor[prerequisites[i][0]]++;
-        }
-        bfs();
-        return (ans.size() == n);
+        return (cnt==n);
     }
 };
