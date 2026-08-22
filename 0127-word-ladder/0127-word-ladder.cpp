357deebb1f449b34e75bdd509282265c524fa94d
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-22 11:55:09 +0530

    Time: 111 ms (32.57%), Space: 23.3 MB (17.24%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,72 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-const long long mod = 1000000009;
-
-#define asc(x) (x).begin(), (x).end()
-#define desc(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define ff first
-#define ss second
-#define forl(i, a, b) for (int i = (a); i < (b); i++)
-#define fore(x, a) for (auto& x : a)
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-// #define endl '\n'
-
 class Solution {
 public:
-    string s1;
-    string s2;
-    int n;
-    vector<string>* arr;
-    unordered_set<string> st;
-
-    int bfs() {
-        // prereq-*adj,n,ans,vis
-
-        // vector<pair<int, int>> padosi{{-1, 0}, {+1, 0}, {0, -1}, {0, +1}};
-        queue<pair<string, int>> q;
-        // starting push and mark it vis:
-        q.push({s1, 1});
-        st.erase(s1);
-
-        // logic:
-        while (!q.empty()) {
-            auto p = q.front();
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,int>vis;
+        unordered_set<string>st;
+        for(auto s:wordList){
+            st.insert(s);
+        }
+        queue<pair<string,int>>q;
+        vis[beginWord]=1;
+        q.push({beginWord,1});
+        while(!q.empty()){
+            auto it = q.front();
             q.pop();
-            string s = p.ff;
-            int d = p.ss;
-
-            
-            for (int i = 0; i < s.size(); i++) {
-
-                for (char c = 'a'; c <= 'z'; c++) {
+            string s = it.first;
+            int d = it.second;
+            for(int i=0;i<s.size();i++){
+                for(char c='a';c<='z';c++){
                     string temp = s;
                     temp[i]=c;
-                    if(st.find(temp)!=st.end()){
-                        st.erase(temp);
+                    if(st.find(temp)!=st.end() && !vis.count(temp)){
+                        vis[temp]=1;
                         q.push({temp,d+1});
-                        if (temp == s2)return d+1;
+                        if(temp==endWord)return d+1;
                     }
                 }
             }
         }
         return 0;
-    }
-
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
-        this->s1 = beginWord;
-        this->s2 = endWord;
-        this->n = wordList.size();
-        this->arr = &wordList;
-        bool flag = false;
-        for(int i=0;i<n;i++){
-            if(wordList[i]==s1)flag = true;
-            st.insert(wordList[i]);
-        }
-        if(!flag)st.insert(s1);
-        return bfs();
     }
 };
