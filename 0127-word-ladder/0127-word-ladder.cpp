ff0ad820b122bb0ab06c94cc834709c5483200fa
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-22 11:59:10 +0530

    Time: 84 ms (37%), Space: 21.4 MB (41.68%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string,int>vis;
         unordered_set<string>st;
         for(auto s:wordList){
             st.insert(s);
         }
         queue<pair<string,int>>q;
-        vis[beginWord]=1;
+        st.erase(beginWord);
         q.push({beginWord,1});
         while(!q.empty()){
             auto it = q.front();
@@ -18,8 +17,8 @@ public:
                 for(char c='a';c<='z';c++){
                     string temp = s;
                     temp[i]=c;
-                    if(st.find(temp)!=st.end() && !vis.count(temp)){
-                        vis[temp]=1;
+                    if(st.find(temp)!=st.end()){
+                        st.erase(temp);
                         q.push({temp,d+1});
                         if(temp==endWord)return d+1;
                     }
