78699e18b3ad1b123931e8fd2a9d6e59288e8311
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-08-23 11:14:46 +0530

    Time: 85 ms (49.57%), Space: 47.8 MB (31.29%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-    int n;
-    int k;
-    vector<vector<pair<int,int>>>adj;
-    vector<int>dist;
-
-    void dijk(){
-        dist.assign(n+1,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        dist[k]=0;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int>dist(n+1,1e9);
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(auto it:times){
+            int a =it[0];
+            int b=it[1];
+            int c=it[2];
+            adj[a].push_back({b,c});
+        }
+        priority_queue<pair<int,int>>pq;
         pq.push({0,k});
+        dist[k]=0;
         while(!pq.empty()){
-            auto it = pq.top();
-            int d = it.first;
-            int node =it.second;
+            auto it =pq.top();
             pq.pop();
-            for(auto x:adj[node]){
-                int newd = x.second;
-                int newnode = x.first;
-                if(d+newd<dist[newnode]){
-                    dist[newnode]=d+newd;
-                    pq.push({dist[newnode],newnode});
+            int node= it.second;
+            int d = it.first;
+            for(auto iit:adj[node]){
+                int nnode = iit.first;
+                int w = iit.second;
+                if(d+w<dist[nnode]){
+                    dist[nnode]=d+w;
+                    pq.push({d+w,nnode});
                 }
             }
         }
-
-    }
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        this->n=n;
-        this->k=k;
-        adj.resize(n+1);
-        for(auto it:times){
-            adj[it[0]].push_back({it[1],it[2]});
-        }    
-        dijk();
-        int maxi = INT_MIN;
-        for(int i=1;i<dist.size();i++){
-            if(dist[i]==INT_MAX)return -1;
-            maxi= max(maxi,dist[i]);
+        int maxi=0;
+        for(int i=1;i<n+1;i++){
+            maxi=max(maxi,dist[i]);
         }
+        if(maxi==1e9)return -1;
         return maxi;
-
     }
 };
