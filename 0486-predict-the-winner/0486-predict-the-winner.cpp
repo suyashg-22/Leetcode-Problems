646744f91d27b0127d719c48b95b5c5864533c1f
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-09-01 19:24:04 +0530

    Time: 0 ms (100%), Space: 9.6 MB (69.24%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int dp[21][21];
     int rec(int l,int r,vector<int>&arr){
+        if(l>r)return 0;
         if(l==r)return arr[l];
         if(dp[l][r]!=-1)return dp[l][r];
-        int maxi=arr[l]-rec(l+1,r,arr);
-        maxi=max(maxi,arr[r]-rec(l,r-1,arr));
-        return dp[l][r]=maxi;
+        int ans = -1e9;
+        ans=max(ans,arr[l]-rec(l+1,r,arr));
+        ans=max(ans,arr[r]-rec(l,r-1,arr));
+        return dp[l][r]=ans;
     }
     bool predictTheWinner(vector<int>& nums) {
-        int n = nums.size();
+        int n= nums.size();
         memset(dp,-1,sizeof(dp));
-        return rec(0,n-1,nums)>=0;
+        int ans = rec(0,n-1,nums);
+        return (ans>=0);
     }
 };
