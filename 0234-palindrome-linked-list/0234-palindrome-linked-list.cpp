5cd546718ecb1b3097ba254650b3c5fdd2fe2cf2
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-09-04 06:24:39 +0530

    Time: 4 ms (48.64%), Space: 118.1 MB (64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,34 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    ListNode* rev(ListNode* head){
+        ListNode* node=head;
+        ListNode* prev = NULL;
+        while(node){
+            ListNode* nxt = node->next;
+            if(prev){
+                node->next=prev;
+            }
+            else{
+                node->next=NULL;
+            }
+            prev=node;
+            node=nxt;
+        }
+        return prev;
+    }
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next) return true;
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* temp = head;
-        ListNode* prev = nullptr;
-
-        // Find middle and reverse first half
-        while(fast && fast->next){
-            fast = fast->next->next;
-
-            // Reverse first half on-the-fly
-            ListNode* frw = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = frw;
+        if(!head->next)return true;
+        ListNode* l = head;
+        ListNode* f = head;
+        while(f->next && f->next->next){
+            l=l->next;
+            f=f->next->next;
         }
-
-        // If odd length, skip middle node
-        if(fast) slow = slow->next;
-
-        // Compare reversed first half with second half
-        while(prev && slow){
-            if(prev->val != slow->val) return false;
-            prev = prev->next;
-            slow = slow->next;
+        ListNode* node1 = head;
+        ListNode* node2 = l->next;
+        l->next=NULL;
+        node2= rev(node2);
+        while(node1 && node2){
+            if(node1->val !=node2->val)return false;
+            node1=node1->next;
+            node2=node2->next;
         }
-
+        if(node1 && !node1->next)return true;
+        if(node2 && !node2->next)return true;
+        if(node1)return false;
+        if(node2)return false;
         return true;
     }
 };
