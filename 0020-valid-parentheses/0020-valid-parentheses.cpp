303e1231a14056fc96de3fcb3c3c9d4a19235310
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-09-06 17:21:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.length();
+        int n= s.size();
         stack<char>st;
-        for(int i=0;i<n;i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-                st.push(s[i]);
+        st.push(s[n-1]);
+        for(int i=n-2;i>=0;i--){
+            char c = s[i];
+            if(c==')' || c=='}' || c==']'){
+                st.push(c);
             }
             else{
-                if(st.empty()) return false;
-                else{
-                    int el = st.top();
+                if(st.empty())return false;
+                char cc = st.top();
+                if(c=='('){
+                    if(cc!=')')return false;
                     st.pop();
-                    if((s[i]==')' && el == '(') || (s[i]==']'&& el=='[') || (s[i]=='}'&& el=='{')) continue;
-                    else return false;
+                }
+                else if(c=='{'){
+                    if(cc!='}')return false;
+                    st.pop();    
+                }
+                else{
+                    if(cc!=']')return false;
+                    st.pop();
                 }
             }
         }
-        if(!st.empty()) return false;
+        if(st.size()>0)return false;
         return true;
     }
 };
