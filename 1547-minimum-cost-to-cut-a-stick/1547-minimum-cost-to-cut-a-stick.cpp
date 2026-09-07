273906595146102ccd90e97a5352fd4e69e3f552
Suyash Ghilahare <suyashghilahare123@gmail.com>
2026-09-07 06:07:42 +0530

    Time: 30 ms (56.3%), Space: 11.3 MB (97.8%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int dp[103][103];
-    int rec(int i,int j,vector<int>&arr){
-        if(j-i<=1)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
+    int rec(int l,int r,vector<int>&arr){
+        if(l+1==r)return 0;
+        if(dp[l][r]!=-1)return dp[l][r];
         int ans=1e9;
-        for(int k=i+1;k<j;k++){
-            ans=min(ans,arr[j]-arr[i]+rec(i,k,arr)+rec(k,j,arr));
+        for(int i=l+1;i<r;i++){
+            ans=min(ans,arr[r]-arr[l]+rec(l,i,arr)+rec(i,r,arr));
         }
-        return dp[i][j]=ans;
+        return dp[l][r]=ans;
     }
     int minCost(int n, vector<int>& cuts) {
-        memset(dp,-1,sizeof(dp));
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
-        int m = cuts.size();
+        memset(dp,-1,sizeof(dp));
+        int m=cuts.size();
         return rec(0,m-1,cuts);
     }
 };
